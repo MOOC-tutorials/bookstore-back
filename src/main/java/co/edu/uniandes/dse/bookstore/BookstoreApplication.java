2ca3456f001e8667c703bf76984ef5edfc62dff4
usuariocoursera1 <jp.reyes39@uniandes.edu.co>
2022-09-07 08:41:16 -0500

    actions with jacoco

--- src/main/java/co/edu/uniandes/dse/bookstore/BookstoreApplication.java
+++ src/main/java/co/edu/uniandes/dse/bookstore/BookstoreApplication.java
@@ -31,7 +31,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookstoreApplication {
 	
 	public static void main(String[] args) {
-		System.out.println("test")
+		System.out.println("test");
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
